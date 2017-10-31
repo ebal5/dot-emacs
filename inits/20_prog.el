84b2baff022ccbcf2aa1e03dd5c52c984d89b9d0
Tani Shuichi <s173342@edu.tut.ac.jp>
2017-10-31 18:23:07 +0900

    setting for js-doc

--- inits/20_prog.el
+++ inits/20_prog.el
@@ -1,3 +1,14 @@
 ;;; Code:
 (add-hook 'prog-mode-hook 'electric-pair-mode)
+(use-package js-doc
+  :config
+  (if (package-installed-p 'hydra)
+      (defhydra my-hydra/jsdoc (global-map "C-!")
+	"For js-doc"
+	("t" js-doc-insert-file-doc)
+	("f" js-doc-insert-function-doc)
+	("s" js-doc-insert-function-doc-snippet)
+	("t" js-doc-insert-tag)
+	))
+  )
 ;;; 20_prog.el ends here
